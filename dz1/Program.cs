7dfeedbcc79517505c9670a321f897b6fd2e56e2
Kashira2 <nekit.egorov@icloud.com>
2022-08-21 14:19:19 +0300

    last commit

---
+++ dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int N1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int N2 =Convert.ToInt32(Console.ReadLine());
+if (N1 > N2)
+{
+    Console.WriteLine(N1 + ">" + N2);
+}
+else
+{
+    Console.WriteLine(N2 + ">" + N1);
+}
